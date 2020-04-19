a35452869195bf5716ef5e4797f7f08662cab5f5
theminer3746 <me@theminerdev.com>
2020-04-19 15:47:27 +0000

    Change Authenticate middleware to use the new authentication system

--- laravel/app/Http/Middleware/Authenticate.php
+++ laravel/app/Http/Middleware/Authenticate.php
@@ -3,19 +3,23 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
 
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @param  \Closure  $next
+     * @return mixed
      */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if ($request->session()->get('auth.logged_in')) {
+            return $next($request);
+        } else {
+            return redirect('/auth/login');
         }
     }
 }
