f24fe82fc6543fdb47d4f72345a265b3a93909bb
theminer3746 <me@theminerdev.com>
2020-04-20 21:11:18 +0000

    Create a blade to show all gateways that belongs to the user

---
+++ laravel/resources/views/gateway/list.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.master')
+
+@section('body')
+    <a href="/gateways/create">
+        <button class="button is-link">
+            Activate a new gateway
+        </button>
+    </a>
+
+    
+@endsection
