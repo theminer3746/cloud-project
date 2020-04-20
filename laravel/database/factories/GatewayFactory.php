d8a9619eca2d69a0e40139053ef1f62c12a31f24
theminer3746 <me@theminerdev.com>
2020-04-20 21:10:32 +0000

    Create Gateway factory

---
+++ laravel/database/factories/GatewayFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\StorageGateway\Gateway;
+use Faker\Generator as Faker;
+
+$factory->define(Gateway::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
