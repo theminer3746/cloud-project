bf5bd6ac4c2cfbee5352043fade647c48beda50f
theminer3746 <me@theminerdev.com>
2020-04-20 21:02:07 +0000

    User now has many gateways

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    public function gateway()
+    {
+        return $this->hasMany('App\StorageGateway\Gateway');
+    }
 }
