b95c4e10b789054ef0e2f196e4702f762e04b49d
theminer3746 <me@theminerdev.com>
2020-04-21 20:25:27 +0000

    Empty the GatewaySeeder

--- laravel/database/seeds/GatewaySeeder.php
+++ laravel/database/seeds/GatewaySeeder.php
@@ -11,13 +11,6 @@ class GatewaySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('gateways')->insert([
-            'user_id' => 1,
-            'activation_key' => bin2hex(random_bytes(10)),
-            'arn' =>  bin2hex(random_bytes(10)),
-            'name' => 'test',
-            'real_name' => 'real_gateway_name',
-            'type' => 'FILE_S3',
-        ]);
+        //
     }
 }
