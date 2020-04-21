75ed09d134456ecbe52bf056af9fe530ff8668e4
theminer3746 <me@theminerdev.com>
2020-04-21 20:25:58 +0000

    Create table to display all gateways that belongs to the user

--- laravel/resources/views/gateway/list.blade.php
+++ laravel/resources/views/gateway/list.blade.php
@@ -7,5 +7,20 @@
         </button>
     </a>
 
-    
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Gateway Name</th>
+                <th>SMB Storage Path</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($gateways as $gateway)
+                <tr>
+                    <th>{{ $gateway['name'] }}</th>
+                    <th>{{ $gateway['path'] }}</th>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
